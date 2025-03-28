361a66e1c7faa30db25ee03d56ed5eaa74fdabc2
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-28 09:38:23 +0100

    +

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 createApp(App).mount('#app')
